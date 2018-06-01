c873d122e9a155e0f84fae9169680e367662a718
= <=>
2018-06-01 03:28:18 +0000

    Remove trailing newline from branch-name
    Adding the quotes meant that, now,
    we'd try to check out "branchname\n",
    which obviously won't work.

--- tico.rb
+++ tico.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 
 selected_branch = `git branch | fzf`
-selected_branch = selected_branch[2..-1]
+selected_branch = selected_branch[2..-1].chomp
 system "git checkout \"#{selected_branch}\""
