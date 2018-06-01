92bb713c043b8b307ee127cefbb650804224387e
= <=>
2018-06-01 03:32:25 +0000

    Exit gracefully if fzf doesn't return a branch-name

--- tico.rb
+++ tico.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
 selected_branch = `git branch | fzf`
+exit if selected_branch.chomp.empty?
 selected_branch = selected_branch[2..-1].chomp
 system "git checkout \"#{selected_branch}\""
